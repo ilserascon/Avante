308f269ec3330ef5df0488062a8f64959579c238
Filiberto Sañudo <filibertosanudo@gmail.com>
2025-05-13 09:21:08 -0700

    Ajustes para los 15 campos en telas e insumos

--- database/migrations/2025_04_18_213932_create_tipo_insumo_table.php
+++ database/migrations/2025_04_18_213932_create_tipo_insumo_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
-
+use Illuminate\Support\Facades\DB;
 return new class extends Migration
 {
     /**
@@ -31,6 +31,26 @@ return new class extends Migration
             $table->string('campo15')->nullable();
             $table->timestamp('created_at')->nullable();
         });
+
+        DB::table('tipo_insumo')->updateOrInsert(
+            ['nombre' => 'Telas'],
+            [
+                'campo1' => 'Diseño',
+                'campo2' => 'Ancho',
+                'campo3' => 'Unidad de Medida',
+                'campo4' => 'Composición',
+                'campo5' => 'Libro',
+                'campo6' => 'Precio CO',
+                'campo7' => 'Precio RO',
+                'campo8' => 'Precio Volumen',
+                'campo9' => 'Modelo',
+                'campo10' => 'Peso',
+                'campo11' => 'Uso',
+                'campo12' => 'Precio Distribuidor',
+                'campo13' => 'Precio',
+                'created_at' => now(),
+            ]
+        );
     }
 
     /**
